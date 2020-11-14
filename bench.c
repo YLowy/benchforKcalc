89b34ecf21fe4d8d72268ec047af04fb1564d900
Ylowy <matt162162162@gmail.com>
2020-11-14 20:16:12 +0800

    Finish the bench test for MathEx vs kcalc-fixed

--- bench.c
+++ bench.c
@@ -1,16 +1,27 @@
 #include "../MathEx/expression.h"
 #include "../MathEx/expression.c"
-
+#include "../kcalc-fixed/fixed-point.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
 #include <sys/time.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <math.h> /* for pow */
 
 int status = 0;
 int fp;
-
+static double eval (fixedp ipt){
+    if (ipt.data == NAN_INT)
+        puts("NAN_INT");
+    else if (ipt.data == INF_INT)
+        puts("INF_INT");
+    else {
+        double evalresult = (int) ipt.inte + ((double) ipt.frac / UINT32_MAX);
+    return evalresult;
+    }
+}
 static float user_func_next(struct expr_func *f, vec_expr_t args, void *c)
 {
     (void)f, (void)c;
@@ -39,28 +50,31 @@ static void test_benchmark(const char *s)
         result = expr_eval(e);
     }
     expr_destroy(e, &vars);
-
-
     //---
-    ssize_t value = write(fp,s,strlen(s));
-    printf("value = %ld\n",value);
+    fixedp kresult = {0};
+    kresult.data = write(fp,s,strlen(s));
+    float kout = eval(kresult);
     //---
+    if(kout == result)
+        printf("BENCH [PASS] %s  MathEx: %.5f kcalc-fixed: %.5f \n ", s, result, kout);
+    else 
+        printf("BENCH [X] %s  MathEx: %.5f kcalc-fixed: %.5f \n ", s, result, kout);
 
-
-
-    printf("BENCH MathEx: %s = %.5f \n", s, result);
 }
 
 int main()
 {
     fp = open("/dev/calc",02);
     if(fp < 0){ 
-        printf("fp =%d\n",fp);
         exit(1);
     }
     printf("---Start Bench---\n");
     
-    test_benchmark("5.6*5.8");
+    test_benchmark("10+1.1");
+    test_benchmark("10*1.1");
+    test_benchmark("10/1.1");
+    test_benchmark("10-1.1");
+    test_benchmark("1.1*1.1");
 
     printf("---End Bench---\n");
     close(fp);
